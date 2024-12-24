b660b65b5daad86f7891ce894acf9a7cc65dee71
kittiwake <kittiwake@mail.ru>
2024-12-24 15:25:55 +0300

    fix: заменяет маркеры на локальные

--- src/constant.tsx
+++ src/constant.tsx
@@ -1,8 +1,6 @@
-export const URL_MARKER_DEFAULT =
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/pin.svg';
+export const URL_MARKER_DEFAULT = '../public/img/pin.svg';
 
-export const URL_MARKER_CURRENT =
-  'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/main-pin.svg';
+export const URL_MARKER_CURRENT = '../public/img/pin-active.svg';
 
 export const cities = [
   'Paris',
